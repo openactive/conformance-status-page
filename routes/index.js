2cc01054ffaef441504f955a7b1a9987bb53aace
Michael Wood <michael.wood@opendataservices.coop>
2020-06-25 20:05:47 +0100

    routes/views: index: Add license info to status table

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,22 @@
 import express from 'express';
 import fetch from 'node-fetch';
+import hbs from 'hbs';
 
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-
   const statusUrl = process.env.STATUS_DATA_URL || "http://localhost:8000/publishers/status";
   let statusData = {};
   let error = undefined;
+
+  hbs.registerHelper('licenseSnippet', function (license) {
+    /* Add more licenses here when needed  */
+    if (license.indexOf("https://creativecommons.org/licenses/by/4.0/") != -1){
+      const template = hbs.compile('<a href="{{license}}"><img src="images/by.png" alt="CC BY 4.0" style="height: 15px" /><br />CC BY 4.0</a>');
+      return template({license : license});
+    }
+  });
 
   try {
     let res = await fetch(statusUrl);
