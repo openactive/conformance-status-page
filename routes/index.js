7cec865888ecc0fdc07d1f7c90110565568d88d2
Michael Wood <michael.wood@opendataservices.coop>
2020-07-20 17:13:46 +0100

    views: publisher_detail: Add module fetch and display publisher details
    This will display further details on a selected publisher on the status
    page.
    
    Also remove a debug console log on app.js

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,13 @@ import hbs from 'hbs';
 
 var router = express.Router();
 
+const conformServerUrl = process.env.CONFORMANCE_SERVER_URL || "http://localhost:8000";
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-  const statusUrl = process.env.STATUS_DATA_URL || "http://localhost:8000/publishers/status";
   let statusData = {};
   let error = undefined;
+
+  const statusUrl = conformServerUrl + '/publishers/status'
 
   hbs.registerHelper('licenseSnippet', function (license) {
     /* Add more licenses here when needed  */
@@ -28,14 +30,43 @@ router.get('/', async function(req, res, next) {
 
   } catch(err) {
     error = err;
+    console.warn(err);
   }
 
   res.render('index', {
     statusData: statusData,
-    statusUrl, statusUrl,
+    statusUrl: statusUrl,
     error: error,
    });
 });
 
+
+router.get('/details_snippet/publisher/:publisherId', async function(req, res, next) {
+  let error = undefined;
+
+  const publisherDetailsUrl = conformServerUrl + '/publisher/'+req.params.publisherId;
+  let publisherDetails;
+
+  try {
+    let res = await fetch(publisherDetailsUrl);
+    if (!res.ok){
+      throw Error(res.status +" : "+ res.statusText);
+    }
+
+    publisherDetails = await res.json();
+
+  } catch(err) {
+    error = err;
+    console.warn(err);
+  }
+
+  res.render('publisher_detail', {
+    publisherDetails: publisherDetails,
+    publisherDetailsUrl: publisherDetailsUrl,
+    error: error,
+    layout: null,
+   });
+
+});
 
 export default router;
